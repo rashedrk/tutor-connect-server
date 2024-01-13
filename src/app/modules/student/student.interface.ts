4ef5e771cc0ab0c64578d4ed4b777ff8e2a57b4d
rashedrk <rashedrk001@gmail.com>
2024-01-13 12:54:15 +0600

    student interface and model added

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+import { Types } from "mongoose"
+
+export type TName = {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+}
+
+export type TAddress = {
+    areaName: string;
+    policeStation: string;
+    postOfficeName: string;
+    postalCode: number;
+    subDistrict: string;
+    district: string;
+    country: string;
+}
+
+export type TGuardian = {
+    fatherName: string;
+    motherName: string;
+    fatherContact: string;
+    motherContact: string;
+}
+
+export type TStudent = {
+    user: Types.ObjectId;
+    name: TName;
+    email: string;
+    gender: 'male' | 'female';
+    dateOfBirth?: string;
+    contactNo: string;
+    presentAddress: TAddress;
+    permanentAddress: TAddress;
+    guardian: TGuardian;
+    profileImage?: string;
+    isDeleted: boolean;
+}
